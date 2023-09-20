218494db470a8c8356e392c89661dc049774cdb1
高巨龙 <gaojulong@outlook.com>
2023-09-20 17:48:23 +0800

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
 # Set desired version to be installed
-VERSION="${VERSION:-master}"
-GITHUB_REPOSITORY=${GITHUB_REPOSITORY:-bluerobotics/blueos-docker}
+VERSION="${VERSION:-alpha}"
+GITHUB_REPOSITORY=${GITHUB_REPOSITORY:-gaojulong/blueos-self}
 REMOTE="${REMOTE:-https://raw.githubusercontent.com/${GITHUB_REPOSITORY}}"
 ROOT="$REMOTE/$VERSION"
+
+DOCKER_VERSION="${DOCKER_VERSION:-1.1.0-alpha.3}"
+BLUEOS_BOOTSTRAP="gaojulong/blueos-bootstrap:$DOCKER_VERSION" # Use current version
+BLUEOS_CORE="gaojulong/blueos-core:$DOCKER_VERSION" # We don't have a stable tag yet
+BLUEOS_FACTORY="gaojulong/blueos-core:factory" # used for "factory reset"
+
+
 alias curl="curl --retry 6 --max-time 15 --retry-all-errors"
 
 # Additional options
@@ -183,9 +190,6 @@ command -v raspi-config && (
 )
 
 echo "Downloading bootstrap"
-BLUEOS_BOOTSTRAP="bluerobotics/blueos-bootstrap:$VERSION" # Use current version
-BLUEOS_CORE="bluerobotics/blueos-core:$VERSION" # We don't have a stable tag yet
-BLUEOS_FACTORY="bluerobotics/blueos-core:factory" # used for "factory reset"
 
 docker pull $BLUEOS_BOOTSTRAP
 docker pull $BLUEOS_CORE
