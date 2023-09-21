7e493ab1390a8d248f47f12c697fd5b25f5dd506
高巨龙 <gaojulong@outlook.com>
2023-09-21 13:59:29 +0800

    开启dockerCI

--- install/install.sh
+++ install/install.sh
@@ -16,7 +16,7 @@ alias curl="curl --retry 6 --max-time 15 --retry-all-errors"
 
 # Additional options
 DO_BOARD_CONFIG=0 # default to do the board config
-RUNNING_IN_CI=0 # default to not running CI mode/docker-in-docker
+RUNNING_IN_CI=1 # default to not running CI mode/docker-in-docker
 
 usage_help()
 {
